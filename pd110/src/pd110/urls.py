252973bc309905ee1d1906dad12d26d1a9d21d4c
cvelaro <34553796+cvelaro@users.noreply.github.com>
2019-05-26 21:14:30 +0200

    Commit 9
    Video 9

--- pd110/src/pd110/urls.py
+++ pd110/src/pd110/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from boletin import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    url(r'^$', views.inicio, name='inicio')
+]	

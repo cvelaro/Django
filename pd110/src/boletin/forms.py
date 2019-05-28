1d4aa3f8279ad9025b502aa2348ce2dca206a975
cvelaro <34553796+cvelaro@users.noreply.github.com>
2019-05-28 22:46:07 +0200

    Commit 17
    Video 17

--- pd110/src/boletin/forms.py
+++ pd110/src/boletin/forms.py
@@ -5,8 +5,20 @@ from .models import Registrados
 class RegModelForm(forms.ModelForm):
 	class Meta:
 		model = Registrados
-		field = ["nombre", "email"]
+		fields = ["nombre", "email"]
 
+    def clean_email(self):
+    	email = self.cleaned_data.get("email")
+    	email_base, proveeder = email.split("@")
+    	dominio, extension = proveedor.split(".")
+    	if not extension == "edu":
+    		raise forms.ValidationError("Por favor utiliza un email con la extension .EDU")
+    	return email
+
+    def clean_nombre(self):
+    nombre = self.cleaned_data.get("nombre")
+    #validaciones
+    return nombre	
 
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=50)

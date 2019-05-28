38a24fd6f69c58611400a8357eb820c75034ec76
cvelaro <34553796+cvelaro@users.noreply.github.com>
2019-05-28 22:12:59 +0200

    Commit 16
    Video 16

--- pd110/src/boletin/admin.py
+++ pd110/src/boletin/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .forms import RegModelForm
 from .models import Registrados
 
 class AdminRegistrados(admin.ModelAdmin):
 	list_display = ["email", "nombre", "timestamp"]
+	form = RegModelForm
 	list_filter = ["timestamp"]
 	# list_display_links = ["nombre"]
 	list_editable = ["nombre"]
 	search_fields = ["email", "nombre"]
-	class Meta:
-		model = Registrados
+	#class Meta:
+	#	model = Registrados
 
 
 admin.site.register(Registrados, AdminRegistrados)
--- pd110/src/boletin/forms.py
+++ pd110/src/boletin/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+from .models import Registrados
+
+class RegModelForm(forms.ModelForm):
+	class Meta:
+		model = Registrados
+		field = ["nombre", "email"]
+
+
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=50)
 	email = forms.EmailField()
